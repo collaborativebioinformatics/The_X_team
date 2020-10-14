3648c5b3d4e8a32aac97b1653b9d3dbc20436cff
Mark Chaisson <mchaisso@usc.edu>
2020-10-14 06:56:04 -0700

    Updated CreateJointSupportTab.sh to add a header to SV calls

--- scripts/CreateJointSupportTab.sh
+++ scripts/CreateJointSupportTab.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-sort -k4,4 $1 > $1.sorted
-sort -k4,4 $2 > $2.sorted
+paste <(echo "ID")  <( head -1 $1) <(head -1 $2 | cut -f 2- | awk '{ s=""; for (i=1;i<NF;i++) { s=s "2_"$i ; if (i<NF-1) { s=s" "; } } print s }') > $3
+tail -n +2 $1 | sort -k4,4  > $1.sorted
+tail -n +2 $2 | sort -k4,4  > $2.sorted
 
-join -1 4 -2 4 $1.sorted $2.sorted | tr " " "\t" | grep -v chrom > $3 
+join -1 4 -2 4 $1.sorted $2.sorted | tr " " "\t"  >> $3
