2633f4cd5425f10e6d957ab559ee31d671d59706
resteban <rocio.esteban@nanoporetech.com>
2021-06-10 15:05:29 +0100

    Fix annotation by reading TP VCF, usage of a second truvari run was removed

--- scripts/AnnotateJointSupportTab.py
+++ scripts/AnnotateJointSupportTab.py
@@ -20,7 +20,7 @@ def readBAM(bam):
 parser = argparse.ArgumentParser( description='Parse FP')
 parser.add_argument('-tab', '--tab', required=False)
 parser.add_argument('-truvariFP', '--fp', required=False)
-parser.add_argument('-truvariFP-Param', '--fpParam', required=False)
+parser.add_argument('-truvariTP', '--tp', required=False)
 parser.add_argument('-hap1', '--hap1', required=False)
 parser.add_argument('-hap2', '--hap2', required=False)
 
@@ -29,16 +29,16 @@ args = parser.parse_args()
 FPList=[]
 bcf_in = VariantFile(args.fp)
 
-FPListExtra=[]
-bcf_in_param = VariantFile(args.fpParam)
+TPList=[]
+bcf_in_tp = VariantFile(args.tp)
 
 for rec in bcf_in.fetch():
     sv_id = rec.id
     FPList.append(rec.id)
 
-for rec in bcf_in_param.fetch():
+for rec in bcf_in_tp.fetch():
     sv_id = rec.id
-    FPListExtra.append(rec.id)
+    TPList.append(rec.id)
 
 bam1 = pysam.AlignmentFile(args.hap1, "rb", header=True)
 bam2 = pysam.AlignmentFile(args.hap2, "rb", header=True)
@@ -51,12 +51,9 @@ tabFile = open(args.tab, "r")
 for line in tabFile.readlines():
     id = line.split("_")[0]
     sv_len = line.split("_")[3]
-    if id in FPList and id in FPListExtra:
-        print("%s\t%s\t%s\t%s\t%s\t%s\t%s" % (line.rstrip("\n"), "FP", "FP-Param", ChrHap1[id][0],  ChrHap1[id][1], ChrHap2[id][0],  ChrHap2[id][1] ))
-    if id in FPList and id not in FPListExtra:
-        print("%s\t%s\t%s\t%s\t%s\t%s\t%s" % (line.rstrip("\n"), "FP", "TP-Param", ChrHap1[id][0],  ChrHap1[id][1], ChrHap2[id][0],  ChrHap2[id][1] ))
-    elif int(sv_len) >= 50:
-        print("%s\t%s\t%s\t%s\t%s\t%s\t%s" % (line.rstrip("\n"), "TP", "TP-Param", ChrHap1[id][0],  ChrHap1[id][1], ChrHap2[id][0],  ChrHap2[id][1] ))
+    if id in FPList:
+        print("%s\t%s\t%s\t%s\t%s\t%s" % (line.rstrip("\n"), "FP", ChrHap1[id][0],  ChrHap1[id][1], ChrHap2[id][0],  ChrHap2[id][1] ))
+    elif id in TPList:
+        print("%s\t%s\t%s\t%s\t%s\t%s" % (line.rstrip("\n"), "TP", ChrHap1[id][0],  ChrHap1[id][1], ChrHap2[id][0],  ChrHap2[id][1] ))
     else:
-        print("%s\t%s\t%s\t%s\t%s\t%s\t%s" % (line.rstrip("\n"), "not_processed", "not_processed", ChrHap1[id][0],  ChrHap1[id][1], ChrHap2[id][0],  ChrHap2[id][1] ))
-
+        print("%s\t%s\t%s\t%s\t%s\t%s" % (line.rstrip("\n"), "not_processed", ChrHap1[id][0],  ChrHap1[id][1], ChrHap2[id][0],  ChrHap2[id][1] ))
